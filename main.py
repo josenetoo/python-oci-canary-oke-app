fc5fcdcc824b0f695092ca3bb07a4e733dafd487
José Neto <53952575+josenetoo@users.noreply.github.com>
2024-06-26 18:13:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    version="0.0"
+    version="1.0"
     namespace = os.getenv('POD_NAMESPACE', default = 'ns-red')
     return {"Message": "DevOps Heroes - OCI Devops","Version":version,"Namespace":namespace}
